25eaf4cf81206bfa28540614b10a6fe71157235f
mariammueenie <mariam_mueen@live.ca>
2026-04-01 11:11:40 -0400

    In client, built home page and cars list with delete and edit actions.

--- Assignment02/car-service-client/src/app/components/cars-list/cars-list.ts
+++ Assignment02/car-service-client/src/app/components/cars-list/cars-list.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { Car } from '../../models/car.model';
+import { CarService } from '../../services/car-service';
 
 @Component({
   selector: 'app-cars-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cars-list.html',
-  styleUrl: './cars-list.css',
+  styleUrl: './cars-list.css'
 })
-export class CarsList {}
+export class CarsList implements OnInit {
+  cars: Car[] = [];
+  errorMessage = '';
+
+  constructor(private carService: CarService) {}
+
+  ngOnInit(): void {
+    this.getCars();
+  }
+
+  getCars(): void {
+    this.carService.getCars().subscribe({
+      next: (data) => {
+        this.cars = data;
+        this.errorMessage = '';
+      },
+      error: () => {
+        this.cars = [];
+        this.errorMessage = 'Could not load cars.';
+      }
+    });
+  }
+
+  deleteCar(id: string): void {
+    const confirmed = confirm('Are you sure you want to delete this car?');
+
+    if (!confirmed) {
+      return;
+    }
+
+    this.carService.deleteCar(id).subscribe({
+      next: () => {
+        this.getCars();
+      },
+      error: () => {
+        this.errorMessage = 'Delete failed.';
+      }
+    });
+  }
+}
